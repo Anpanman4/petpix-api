7d3dd0603faf4ec416267abca06866ac42690d9b
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-10-30 18:42:41 +0300

    feat: do likes

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import Post from "../models/post";
 
 import { RequestWithUser } from "../types/reqWithUser";
+import NotFoundError from "../errors/notFoundError";
 
 export const getPosts = (req: Request, res: Response, next: NextFunction) => {
   Post.find({})
@@ -34,6 +35,36 @@ export const createPost = (req: RequestWithUser, res: Response, next: NextFuncti
     })
     .catch(err => {
       if (err.name === "ValidationError") next(new SyntaxError("Переданы некорректные данные при создании карточки."));
+      next(err);
+    });
+};
+
+export const doLike = (req: RequestWithUser, res: Response, next: NextFunction) => {
+  const { postId } = req.params;
+
+  Post.findByIdAndUpdate(postId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .populate(["owner", "likes"])
+    .then(post => {
+      if (post) return res.send(post);
+      return next(new NotFoundError("Карточка по ID не найдена"));
+    })
+    .catch(err => {
+      if (err.name === "CastError") next(new SyntaxError("Передан невалидный ID"));
+      next(err);
+    });
+};
+
+export const removeLike = (req: RequestWithUser, res: Response, next: NextFunction) => {
+  const { postId } = req.params;
+
+  Post.findByIdAndUpdate(postId, { $pull: { likes: req.user._id } }, { new: true })
+    .populate(["owner", "likes"])
+    .then(post => {
+      if (post) return res.send(post);
+      return next(new NotFoundError("Карточка по ID не найдена"));
+    })
+    .catch(err => {
+      if (err.name === "CastError") next(new SyntaxError("Передан невалидный ID"));
       next(err);
     });
 };
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getPosts, getPostsMe, createPost } from "../controllers/posts";
+import { getPosts, getPostsMe, createPost, doLike, removeLike } from "../controllers/posts";
 
 const postRouter: express.Router = express.Router();
 
 postRouter.get("", getPosts);
 postRouter.get("/me", getPostsMe);
 postRouter.post("", createPost);
+postRouter.put("/:postId/likes", doLike);
+postRouter.delete("/:postId/likes", removeLike);
 
 export default postRouter;
